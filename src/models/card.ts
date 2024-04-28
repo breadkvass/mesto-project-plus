eaf76b152f67f982e8ec2957869c0206a2d6fb2f
Anastasiya <sulimova.a.m@yandex.ru>
2024-04-28 14:19:31 +0300

    add card Schema and model

---
+++ src/models/card.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+interface Card {
+  name: string;
+  link: string;
+  owner: mongoose.Types.ObjectId;
+  likes: mongoose.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const cardSchema = new mongoose.Schema<Card>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model<Card>('card', cardSchema);
