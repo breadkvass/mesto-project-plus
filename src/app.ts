551e2fc588d38200b2034e73d9f158c721f9d0ce
Anastasiya <sulimova.a.m@yandex.ru>
2024-04-30 16:20:30 +0300

    upd mongoose connect

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import usersRouter from './routes/user';
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+async function start() {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect('mongodb://localhost:27017', {
+      dbName: 'mestodb',
+      user: 'admin',
+      pass: 'admin',
+    });
+    app.use('/users', usersRouter);
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
