5585aa5911e267fa97278709ae0cc5ae31e33470
Anastasiya <sulimova.a.m@yandex.ru>
2024-05-01 16:27:46 +0300

    add errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import usersRouter from './routes/user';
+import cardsRouter from './routes/card';
+import { NotFoundError } from './types/errors';
+import { errorHandler } from './middleware/errors';
 
 const { PORT = 3000 } = process.env;
 
@@ -18,6 +21,19 @@ async function start() {
       pass: 'admin',
     });
     app.use('/users', usersRouter);
+    app.use('/cards', cardsRouter);
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      req.user = {
+        _id: '6630ef28bcdc68e00ab0f609',
+      };
+      next();
+    });
+
+    app.use((_req, _res, next) => {
+      next(new NotFoundError('Запрашиваемый ресурс не найден'));
+    });
+    app.use(errorHandler);
     app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
   } catch (error) {
     console.log(error);
