49b9086353bb02d5b1c4cbcf59532a82a88dc51a
Anastasiya <sulimova.a.m@yandex.ru>
2024-05-02 16:59:41 +0300

    add likeCard and unLikeCard to routes

--- src/routes/card.ts
+++ src/routes/card.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getCards, createCard, deleteCard } from '../controllers/card';
+import { getCards, createCard, deleteCard, likeCard, unlikeCard } from '../controllers/card';
 
 const router = Router();
 
 router.get('/', getCards);
 router.delete('/:cardId', deleteCard);
 router.post('/', createCard);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', unlikeCard);
 
 export default router;
