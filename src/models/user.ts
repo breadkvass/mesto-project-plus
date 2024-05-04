ee7bd248744e931d0a806e0162d4b0564071a1fa
Anastasiya <sulimova.a.m@yandex.ru>
2024-05-04 15:56:13 +0300

    add email, password to user scheme, upd name, about and avatar

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,12 @@
 import { model, Schema, Document } from 'mongoose';
+import validator from 'validator';
 
 export interface IUser extends Document {
   name: string;
   about: string;
   avatar: string;
+  email: string;
+  password: string;
 }
 
 export interface IUserId extends Document {
@@ -16,17 +19,34 @@ const userSchema = new Schema<IUser>({
     required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 200,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
     required: true,
+    validate: {
+      validator: (value: string) => validator.isURL(value),
+      message: '{VALUE} is not a valid URL',
+    },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value: string) => validator.isEmail(value),
+      message: '{VALUE} is not a valid email',
+    },
+  },
+  password: { type: String, required: true, select: false },
 });
 
 const User = model<IUser>('user', userSchema);
